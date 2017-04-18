d8b63cce1d43ec930e7daa9ec5d182d2baa5998b
falcont <tsokolp@gmail.com>
2017-04-19 02:32:28 +0300

    add questions#show & #new spec

--- spec/controllers/questions/index.rb
+++ spec/controllers/questions/index.rb
@@ -4,9 +4,8 @@ RSpec.describe QuestionsController, type: :controller do
   describe 'GET #index' do
     let(:questions) { create_list(:question, 2) }
 
-    before do
-      get :index
-    end
+    before { get :index }
+    
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
