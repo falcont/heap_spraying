201863be839e2713416ced9b142ad863d782cde0
falcont <tsokolp@gmail.com>
2017-04-26 00:34:22 +0300

    edit question controller specs

--- spec/controllers/questions/index_spec.rb
+++ spec/controllers/questions/index_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe QuestionsController, type: :controller do
-  describe 'GET #index' do
-    let(:questions) { create_list(:question, 2) }
+  
+  let(:user) { create(:user) }
+  let(:questions) { create_list(:question, 2, user: user) }
+  before { get :index }
 
-    before { get :index }
-    
+  describe 'GET #index' do
+    sign_in_user
 
     it 'populates an array of all questions' do
       expect(assigns(:questions)).to match_array(questions)
@@ -15,4 +17,17 @@ RSpec.describe QuestionsController, type: :controller do
       expect(response).to render_template :index
     end
   end
+
+  describe 'GET #index for unlogine user' do
+    it 'populates an array of all questions' do
+      expect(assigns(:questions)).to match_array(questions)
+    end
+
+    it 'renders index view' do
+      expect(response).to render_template :index
+    end
+  end
+
+
+
 end
