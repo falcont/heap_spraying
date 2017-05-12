a281716460336d0ea796173d902a27e477588096
falcont <tsokolp@gmail.com>
2017-05-12 20:37:51 +0300

    Question accepts nested attr for attachmets

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,8 @@ class Question < ApplicationRecord
   has_many :answers, dependent: :destroy
   has_many :attachments
   belongs_to :user
+
   validates :title, :body, presence: true
+
+  accepts_nested_attributes_for :attachments
 end
