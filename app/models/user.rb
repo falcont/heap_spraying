22f0332212c1fcb9172f9f6a8ec9be9de97dca52
Taras Sokol <tsokolp@gmail.com>
2017-11-19 01:43:42 +0300

    add oauth user model spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,15 @@ class User < ApplicationRecord
 
   has_many :questions, dependent: :destroy
   has_many :answers, dependent: :destroy
-
+  has_many :authorizations
 
   def author?(obj)
     self.id == obj.user_id
+  end
+
+  def self.find_for_oauth(auth)
+    authorization = Authorization.where(provider: auth.provider, uid: auth.uid.to_s).first
+    authorization.user if authorization
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -17,6 +17,19 @@ RSpec.describe User, type: :model do
     it 'user is not author' do 
       expect(user2).to_not eq question.user
     end
+  end
 
+
+  describe '.find_for_oauth' do 
+    let!(:user) { create(:user) }
+    let(:auth) { OmniAuth::AuthHash.new(provider: 'facebook', uid: '123456') }
+
+    context 'user already has authorization' do 
+      it 'returns the user' do 
+        user.authorizations.create(provider: 'facebook', uid: '123456')
+        expect(User.find_for_oauth(auth)).to eq user
+      end
+    end
+    
   end
 end
