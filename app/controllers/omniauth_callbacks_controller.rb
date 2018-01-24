298e13cb6aa9be4f94981f8789214a05291d0c3c
Taras Sokol <tsokolp@gmail.com>
2018-01-24 23:32:12 +0300

    fix omniauth_callbacks_controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -15,6 +15,8 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: name) if is_navigational_format?
+    else
+      redirect_to new_user_registration_url
     end  
   end
 end
