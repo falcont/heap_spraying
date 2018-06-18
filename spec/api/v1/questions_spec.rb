86c1278c4d01c0a013224284688a483ed49a746d
Taras Sokol <tsokolp@gmail.com>
2018-06-19 00:42:54 +0300

    include answers in questions api

--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -17,13 +17,15 @@ describe 'Questions API' do
     context 'authorized' do 
       let(:access_token) { create(:access_token) }
       let!(:questions) { create_list(:question, 2) }
-      before { get '/api/v1/questions', format: :json, access_token: access_token.token }
+      let(:question) { questions.first  }
+      let!(:answer) { create(:answer, question: question) }
 
+
+      before { get '/api/v1/questions', format: :json, access_token: access_token.token }
 
       it 'returns 200 status code' do 
         expect(response).to be_success
       end
-
 
       it 'returns list of questions' do 
         expect(response.body).to have_json_size(2)
@@ -31,8 +33,19 @@ describe 'Questions API' do
 
       %w(id title body created_at updated_at).each do |attr|
         it "question object contains #{attr}" do 
-          question = questions.first 
           expect(response.body).to be_json_eql(question.send(attr.to_sym).to_json).at_path("0/#{attr}")
+        end
+      end
+
+      context 'answers' do        
+        it 'included in question object' do 
+          expect(response.body).to have_json_size(1).at_path("0/answers")
+        end
+        
+        %w(id body created_at updated_at).each do |attr|
+          it "question object contains #{attr}" do 
+            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/answers/0/#{attr}")
+          end
         end
       end
     end
